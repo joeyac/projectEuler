277af343f98366982d6fc96fa8ef58c187a4a51e
crazyX <623353308@qq.com>
2019-11-04 00:25:46 +0800

    update p076 solution code && key

--- p076.py
+++ p076.py
@@ -1,9 +1,15 @@
 import math
-# https://brilliant.org/wiki/partition-of-an-integer/
-# https://math.stackexchange.com/questions/2675382/calculating-integer-partitions
+
+def arr1(x):
+    return x * (3 * x - 1) // 2
+
+def arr2(x):
+    return x * (3 * x + 1) // 2
 
 def arr(x):
-    return x * (3 * x  - 1) // 2
+    return [arr1(x), arr1(-x)]
+    # equal:
+    # return [arr1(x), arr2(x)]
 
 N = 100
 ans = [1]
@@ -11,10 +17,26 @@ for i in range(1, N + 1):
     ans.append(0)
     for k in range(1, i + 1):
         coeff = (-1) ** (k + 1)
-        for t in [arr(k), arr(-k)]:
+        for t in arr(k):
             if (i - t) >= 0:
                 ans[i] += coeff * ans[i - t]
 
 for i in range(1, 6 + 1):
     print(ans[i] - 1)
 print(ans[100] - 1)
+
+
+dp = {}
+def func(x, y):
+    if (x,y) in dp:
+        return dp[(x,y)]
+    if x == 1 or y == 1:
+        return 1
+    if x < y:
+        dp[(x,y)] = func(x, x)
+    elif x == y:
+        dp[(x,y)] = func(x, x - 1) + 1
+    else:
+        dp[(x,y)] = func(x - y, y) + func(x, y - 1)
+    return dp[(x,y)]
+print(func(100, 100) - 1)
