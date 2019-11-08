910deae09216d6c853a056f07828a1234ea4cae4
joeyac <623353308@qq.com>
2019-11-08 19:19:44 +0800

    update cpp base file

--- snippets/base.cpp
+++ snippets/base.cpp
@@ -6,6 +6,9 @@ using namespace std;
 using namespace std::chrono;
 #endif
 
+typedef long long ll;
+typedef pair<int,int> PI;
+
 void readInput() {
 
 }
@@ -17,17 +20,20 @@ void doProcess() {
 int main() {
 #ifndef ONLINE_JUDGE
     freopen("data.in", "r", stdin);
-    auto start = high_resolution_clock::now();
 #endif
 
     readInput();
+
+#ifndef ONLINE_JUDGE
+auto start = high_resolution_clock::now();
+#endif
     doProcess();
 
 #ifndef ONLINE_JUDGE
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken by solution: "
-         << duration.count() << " microseconds" << endl;
+         << duration.count() / 1000000.0 << " seconds" << endl;
 #endif
     return 0;
 }
